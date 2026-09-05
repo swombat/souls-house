0aadc7f5aa628848ec52974e48426238bcba3300
swombat <daniel.github@tenner.org>
2026-09-05 18:48:20 +0200

    Set up Bundler before loading local-instance dependencies

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,8 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
+require "bundler/setup" # Select locked gems before local-instance helpers load JSON.
+
 require_relative "local_instance"
 LocalInstance.current.claim!
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
